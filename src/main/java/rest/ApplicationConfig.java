0823722033291f297de1f5f831622f48fe243c38
Matias Rottbøll <mrottboell@gmail.com>
2023-04-19 16:11:11 +0200

    Here goes nothing

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -25,12 +25,12 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.API_ExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(UserResource.class);
-        resources.add(RenameMeResource.class);
-        resources.add(JokesResource.class);
-        resources.add(BugReportRessource.class);
-        resources.add(CountryRessource.class);
-        resources.add(DifficultyCheckerRessource.class);
+        resources.add(rest.UserResource.class);
+        resources.add(rest.RenameMeResource.class);
+        resources.add(rest.JokesResource.class);
+        resources.add(rest.BugReportRessource.class);
+        resources.add(rest.CountryRessource.class);
+        resources.add(rest.DifficultyCheckerRessource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
